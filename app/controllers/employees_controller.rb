c7326981e33a02d6ade4cbe5882ec9a54ec7d1d6
juanraba <jrabanillog@gmail.com>
2018-09-12 13:36:23 -0300

    employee create

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,12 @@
 class EmployeesController < ApplicationController
+  def create
+    @employee = Employee.new(employee_params)
+    redirect_to @employee.company if @employee.save
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :email)
+  end
 end
